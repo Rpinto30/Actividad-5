d99fa6fc59e327b7f1cb7b10efd84470231ecb0d
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-22 09:48:08 -0600

    Adios opcion 6

--- actividad_5.py
+++ actividad_5.py
@@ -3,8 +3,8 @@ sales = []
 while True:
     print("-"*10+"Bienvenido!"+"-"*10)
     print("  1. Ingresar lista de ventas\n  2. Mostrar todas las ventas ingresadas\n  3. Calcular la venta más alta y la más baja"+
-          "\n  4. Calcular promedio de ventas\n  5. Contar cuántos días superaron los Q1000\n  6. Buscar venta"+
-          "\n  7. Clasificar cada venta\n  8. Salir")
+          "\n  4. Calcular promedio de ventas\n  5. Contar cuántos días superaron los Q1000"+
+          "\n  6. Clasificar cada venta\n  7. Salir")
     op = input("Ingrese una de las opciones: ")
 
     match op:
